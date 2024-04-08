943b8ae9bf373dc2acfd634329719965fa9d63df
Chuqol <151944573+chuqol@users.noreply.github.com>
2024-04-08 05:06:40 +0000

    Fixup after A14 QPR2 merge [2/2]

--- src/org/evolution/oplus/OPlusExtras/refreshrate/RefreshActivity.java
+++ src/org/evolution/oplus/OPlusExtras/refreshrate/RefreshActivity.java
@@ -20,7 +20,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+import com.android.settingslib.collapsingtoolbar.R;
 
 public class RefreshActivity extends CollapsingToolbarBaseActivity {
 
@@ -30,7 +30,7 @@ public class RefreshActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new RefreshSettingsFragment(), TAG_REFRESH).commit();
     }
 
